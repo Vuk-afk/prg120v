9b424c387c55d07e306706e3b9ab851032a83748
Vuk-afk <vuk.vukovic111@gmail.com>
2025-09-22 12:54:20 +0200

    sss

--- ovingsoppgave-1/oppgave-1.php
+++ ovingsoppgave-1/oppgave-1.php
@@ -3,8 +3,8 @@
 /*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
 /*    Programmet skriver ut en "god dag"-melding med personens navn 
 */
-  $fornavn=$_POST ["fornavn"];
-  $etternavn=$_POST ["etternavn"];  
+  $fornavn=$_POST ["Vuk"];
+  $etternavn=$_POST ["Stevanovic"];  
 	
   print ("God dag $fornavn $etternavn <br />");  
 ?>
