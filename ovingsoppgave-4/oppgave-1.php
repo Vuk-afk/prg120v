25daff19ece91fabdb643b7051552f74a13879b9
Vuk-afk <vuk.vukovic111@gmail.com>
2025-09-24 11:29:51 +0200

    ddd

--- ovingsoppgave-4/oppgave-1.php
+++ ovingsoppgave-4/oppgave-1.php
@@ -1,19 +1,26 @@
-<?php    /* Eksempel 1 */
-/*
-/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
-/*    Programmet lager et fullt navn ved bruk av en egendefinert fuksjon 
-*/
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Eksempel 1</title>
+</head>
+<body>
+  <h3>Eksempel 1</h3>
 
-function fulltNavn($fornavn,$etternavn)
-{
-  $navn=$fornavn . " " . $etternavn;	
-  return $navn; 	
-}
+  <?php
+  // sjekk om skjemaet er sendt inn
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $fornavn = $_POST["fornavn"];
+      $etternavn = $_POST["etternavn"];
 
-  $fornavn=$_POST ["fornavn"];
-  $etternavn=$_POST ["etternavn"];  
+      echo "God dag $fornavn $etternavn <br />";
+  }
+  ?>
 
-  $navn=fulltNavn($fornavn,$etternavn);
-
-  print ("Navnet er $navn");  
-?>
+  <form method="post" action="">
+    Fornavn <input type="text" id="fornavn" name="fornavn" required /> <br />
+    Etternavn <input type="text" id="etternavn" name="etternavn" required /> <br />
+    <input type="submit" value="Fortsett" id="fortsett" name="fortsett" />
+    <input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
+  </form>
+</body>
+</html>
